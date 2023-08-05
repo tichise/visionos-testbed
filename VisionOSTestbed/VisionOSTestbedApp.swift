2e74400f7375fbc23940382ed6145933a046f7b0
tichise <43707+tichise@users.noreply.github.com>
2023-08-05 15:45:28 +0900

    ウインドウサイズを変更

--- VisionOSTestbed/VisionOSTestbedApp.swift
+++ VisionOSTestbed/VisionOSTestbedApp.swift
@@ -12,13 +12,13 @@ struct VisionOSTestbedApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-        }
+        }.defaultSize(width: 0.5, height: 0.5, depth: 0.1, in: .meters)
         
         WindowGroup(id: WindowGroupId.settings.rawValue) {
             SubContentView()
         }
         .windowStyle(.automatic)
-        .defaultSize(width: 0.5, height: 0.5, depth: 0.1, in: .meters)
+        .defaultSize(width: 1, height: 0.5, depth: 0.1, in: .meters)
 
         ImmersiveSpace(id: "ImmersiveSpace") {
             ImmersiveView()
