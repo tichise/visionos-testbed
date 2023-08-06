2f412798be86a6dd2e9f614c6a501ae9ae0d869c
tichise <43707+tichise@users.noreply.github.com>
2023-08-06 12:04:56 +0900

    ChatGPTUIを追加

--- VisionOSTestbed/View/ChatContentView.swift
+++ VisionOSTestbed/View/ChatContentView.swift
@@ -8,24 +8,17 @@
 import SwiftUI
 import RealityKit
 import RealityKitContent
+import OpenAI
+import ChatGPTUI
 
 struct ChatContentView: View {
-    
-    // ChatViewModelを利用する
-    @ObservedObject var chatViewModel = ChatViewModel()
-    
+
+    var chatViewModel = ChatViewModel(
+        openAIClient: OpenAI(apiToken: UserDefaults.standard.string(forKey: AppConstants.chatGPTSecretKey) ?? "")
+    )
+
     var body: some View {
-        VStack {
-            // SubContentViewを別のWindowで起動するボタン
-            Button("Send Chat") {
-                chatViewModel.sendChat(content: "Hello")
-            }
-        }.onAppear(perform: {
-            // ChatViewModelのapiTokenにAppStorageで保存した値をapiTokenに設定する
-            let apiToken = UserDefaults.standard.string(forKey: AppConstants.chatGPTSecretKey) ?? ""
-            
-            chatViewModel.apiToken = apiToken
-        })
+        ChatView(chatViewModel: chatViewModel)
         .padding()
         .navigationTitle("Chat")
     }
